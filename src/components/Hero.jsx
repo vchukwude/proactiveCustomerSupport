ca652345784db60975063555ef14b641cbf9337b
vchukwude <vchukwude@yahoo.com>
2023-09-18 00:47:56 +0100

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = ({ children }) => {
 				<div class='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
 					<div class='mr-auto place-self-center lg:col-span-7'>
 						<h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-							Predictive System for companies
+							Predictive System
 						</h1>
 				
 						{!token && (
