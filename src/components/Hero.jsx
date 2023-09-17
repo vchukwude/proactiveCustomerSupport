68a8c8a172a3395e43a96bd4f8299dacf875882e
vchukwude <vchukwude@yahoo.com>
2023-09-18 00:43:27 +0100

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,13 +16,9 @@ const Hero = ({ children }) => {
 				<div class='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
 					<div class='mr-auto place-self-center lg:col-span-7'>
 						<h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-							Predictive tool for companies
+							Predictive System for companies
 						</h1>
-						<p class='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-							Quibusdam expedita cumque alias repudiandae obcaecati deleniti
-							ghis lorem ipus hinmat
-						</p>
+				
 						{!token && (
 							<a
 								href='/register'
