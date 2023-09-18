a9e2f4910b6a6788c09f521cc7d3993890456a81
vchukwude <vchukwude@yahoo.com>
2023-09-18 21:49:02 +0100

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
 									href='/login'
 									className='text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800'
 								>
-									Hi,{user}
+									Hi, user-{user}
 								</Link>
 
 								<span
@@ -118,7 +118,7 @@ const Header = () => {
 									to='/complaints'
 									className='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-indigo-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'
 								>
-									Complaints
+									Predictions
 								</Link>
 							</li>
 						
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -78,11 +78,11 @@ const Signup = () => {
 							Staff ID
 						</label>
 						<input
-							type='text'
+							type='number'
 							name='username'
 							id='username'
 							className='bg-gray-50 border border-gray-300 text-gray-900  text-lg  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-							placeholder='john.doe'
+							placeholder='0000'
 							required
 							value={formData.username}
 							onChange={handleChange}
